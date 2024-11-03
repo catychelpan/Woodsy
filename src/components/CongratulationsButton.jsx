6f3bf7cbd1386bc6320cf707ecf84e9f07fea630
Kateryna Chelpan <catychelpan@gmail.com>
2024-11-03 12:56:38 +0200

    Images issues fix

--- src/components/CongratulationsButton.jsx
+++ src/components/CongratulationsButton.jsx
@@ -2,12 +2,15 @@
 /* eslint-disable react/prop-types */
 
 
+import Treasure from "../assets/landing/treasure-box.png"
+
+
 function CongratulationsButton({onOpen}) {
 
 
   return (
     <div onClick={onOpen} className='max-w-[400px] relative cursor-pointer lg:top-[-1150px] lg:left-[1100px]'>
-      <img src="../../public/landing/treasure-box.png" alt="treasure box" />
+      <img src={Treasure} alt="treasure box" />
     </div>
   )
 }
